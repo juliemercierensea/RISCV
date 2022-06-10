4a8a80726e4cc499b59fc9b52d3b7b08f8bc2f8a
juliemercierensea <95049893+juliemercierensea@users.noreply.github.com>
2022-06-10 16:25:39 +0200

    Add files via upload

---
+++ Control_Unit/Instruction.h
@@ -0,0 +1,35 @@
+/*!
+ * \file Instruction.h
+ * \brief Extract the values of func10_0, func7_5, func3 ad opcode_6to2 from the instruction signal (32bits)\n
+ * \author Julie Mercier
+ * \version 0.1
+ * \date  9 June 2022\n
+ */
+
+#ifndef INSTRUCTION_H
+#define INSTRUCTION_H
+
+#include <systemc.h>
+
+SC_MODULE (scInstruction) {
+
+    sc_in<sc_lv<32>>     instruction;
+
+    sc_out<bool>         func12_0;
+    sc_out<bool>         func7_5;
+    sc_out<sc_lv<3>>     func3;
+    sc_out<sc_lv<5>>     opcode_6to2;
+
+    SC_CTOR(scInstruction) {
+        SC_THREAD(selection);
+        sensitive << instruction;
+    }
+    void selection (){
+        func12_0.write((instruction.read().to_uint()&0x100000));
+        func7_5.write((instruction.read().to_uint()&0x40000000));
+        func3.write((instruction.read().to_uint()&0x7000));
+        opcode_6to2.write((instruction.read().to_uint()&0x7C));
+    }
+};
+
+#endif // INSTRUCTION_H
