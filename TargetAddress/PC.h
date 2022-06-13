e6b78213714a1bcb0f3d860fb1de9f2320cc06be
juliemercierensea <95049893+juliemercierensea@users.noreply.github.com>
2022-06-13 12:16:09 +0200

    Add files via upload

--- TargetAddress/PC.h
+++ TargetAddress/PC.h
@@ -5,60 +5,45 @@
 SC_MODULE (scPC) {
     // ---------------------      Ports      ---------------------
 
-    sc_in<sc_lv<32>>    targetPC{"LoadVal_targetPC_i"};
-    sc_in<sc_lv<32>>    wPC{"Load_wPC_i"};
-    sc_in<bool>    iPC{"Inc"};
-    sc_in<bool>    RST{"reset"};
+    sc_in<sc_lv<32>>    LoadVal{"LoadVal_targetPC_i"};
+    sc_in<sc_lv<32>>    Load{"Load_wPC_i"};
+    sc_in<bool>    Inc{"iPC_i"};
+    sc_in<bool>    RST{"reset_i"};
     sc_clock    clk;
 
     sc_out<sc_lv<32>>    PC_value{"Count_o"};
 
     SC_CTOR(scPC) {
-        SC_THREAD(name);
-        sensitive << targetPC;
-        sensitive << wPC;
+        SC_THREAD(PClogic);
+        sensitive << LoadVal;
+        sensitive << Load;
         sensitive << clk;
         sensitive << RST;
-        sensitive << iPC;
+        sensitive << Inc;
     }
 
-    void name() {
-        PC_value.write(0); //on initialise à 0 (début fetch)
-        while(1){
+    void PClogic() {
 
-            if (RST.read() == true) {
-                PC_value.write(0); // est ce qu'il ne se passe que ça?
-                }
-
-            if(wPC.read().to_uint()==0){//on fait +4 tant que ? absence de wPC ie wPC==0 (?)
-               PC_value.write(PC_value.read().to_uint()+4);
-               }
-            else {
-               PC_value.write(wPC.read()); //PC_value prend la valeur de wPC pour prendre le branchement
-               }
-
-            wait();
-        }
-//autre compréhension :
         PC_value.write(0); //on initialise
         while(1){
 
             if (RST.read() == true) {
-                PC_value.write(targetPC.read()); // on initialise à la valeur targetPC
+                PC_value.write(0); // on réinitialise
+            }
 
-                if(wPC.read().to_uint()==0){//on fait +4 tant que ? absence de wPC ie wPC==0 (?)
-                    PC_value.write(PC_value.read().to_uint()+4);
+            if(Inc.read()==1){//on fait +4 tant que Inc=1
+                PC_value.write(PC_value.read().to_uint()+4);
                 }
-                }
+
             else {
-                if(wPC.read().to_uint()==0){//on fait +4 tant que ? absence de wPC ie wPC==0 (?)
-                    PC_value.write(PC_value.read().to_uint()+4);
+                if(Load.read()==1){
+                    PC_value.write(LoadVal);
                 }
             }
             wait();
         }
+    }
 
-     }
 };
 
 #endif // WRITEPC_H
